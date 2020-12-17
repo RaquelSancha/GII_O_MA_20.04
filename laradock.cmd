118c8365acbe2aefe798d6878f7a6c70234828c4
RaquelSancha <33413581+RaquelSancha@users.noreply.github.com>
2020-12-17 16:41:48 +0100

    Update laradock.cmd

--- laradock.cmd
+++ laradock.cmd
@@ -1,3 +1,1 @@
-git clone https://github.com/Laradock/laradock.git
-cp env-example .env
-docker-compose up -d apache2 mysql phpmyadmin php-fpm workspace selenium
+docker-compose up -d apache2 mysql phpmyadmin php-fpm workspace selenium sonarqube
